cbc87a6da05694ffc505a653fec2474718dc5c6c
Auburngrads <auburngrads@live.com>
2018-03-20 18:52:14 -0400

    Update file_getter.R

--- file_getter.R
+++ file_getter.R
@@ -1,7 +1,7 @@
 library(XML)
 library(rvest)
 library(RCurl)
-
+library(rprojroot)
 
 url <- 'http://bobby.gramacy.com/teaching/dataworks_rsm'
 
@@ -14,3 +14,11 @@ url_parsed <- htmlParse(rcurl.doc, asText = TRUE)
 links <- xpathApply(url_parsed, "//a", XML::xmlValue)
 
 files <- paste0(url,'/', gsub(' ','',links[-1]))
+
+root <- find_root(is_rstudio_project)
+
+for(i in files) {
+  dir <- 'short_course'
+  base <- basename(i)
+  download.file(i, destfile = file.path(root,dir,base))
+}
